780edff6540bcaddf0badc70ac78f1aef15e487b
dobos <doboszdav@gmail.com>
2023-10-10 08:53:17 +0200

    google code style

--- be/src/main/java/pl/dobos/notemax/NotemaxApplication.java
+++ be/src/main/java/pl/dobos/notemax/NotemaxApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class NotemaxApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(NotemaxApplication.class, args);
-	}
+  public static void main(String[] args) {
+    SpringApplication.run(NotemaxApplication.class, args);
+  }
 
 }
