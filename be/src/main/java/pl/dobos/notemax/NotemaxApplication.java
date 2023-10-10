30fd042795377aeaadef786bab2ef0c283595dfe
dobos <doboszdav@gmail.com>
2023-10-10 08:47:48 +0200

    initial backend setup

---
+++ be/src/main/java/pl/dobos/notemax/NotemaxApplication.java
@@ -0,0 +1,13 @@
+package pl.dobos.notemax;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NotemaxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NotemaxApplication.class, args);
+	}
+
+}
---
+++ be/src/test/java/pl/dobos/notemax/NotemaxApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.dobos.notemax;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotemaxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
