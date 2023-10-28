320505d1bff72def549b452dec3adebb162e8273
dobos <doboszdav@gmail.com>
2023-10-28 19:05:51 +0200

    home page added

--- fe/src/app/page.tsx
+++ fe/src/app/page.tsx
@@ -1,9 +1,19 @@
 import Container from '@mui/material/Container'
+import Typography from "@mui/material/Typography/Typography";
+import Button from "@mui/material/Button";
 
 export default function Home() {
   return (
-    <Container component="main" maxWidth="xl">
-      HELLO
-    </Container>
+      <Container sx={{ textAlign: 'center', mt: 10 }}>
+        <Typography variant="h2" color="primary" sx={{ mb: 4 }}>
+          Notes App
+        </Typography>
+        <Typography variant="h5" color="textSecondary" sx={{ mb: 6 }}>
+          Effortlessly organize your thoughts and ideas.
+        </Typography>
+        <Button variant="contained" color="primary" size="large" href={'/notes/my-notes'}>
+          Get Started
+        </Button>
+      </Container>
   )
 }
